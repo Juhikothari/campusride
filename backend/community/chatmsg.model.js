e2e4c06ad70f9cc9abe18e7417e1f06d493405ed
Juhikothari <juhikthr@gmail.com>
2026-08-03 21:46:12 +0530

    Add files via upload

--- backend/community/chatmsg.model.js
+++ backend/community/chatmsg.model.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const chatMsgSchema = new mongoose.Schema({
-  college:  { type: String, required: true, index: true },
-  sender:   { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  message:  { type: String, required: true, maxlength: 500 },
+  college:   { type: String, required: true, index: true },
+  sender:    { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  message:   { type: String, required: true, maxlength: 500 },
+  anonymous: { type: Boolean, default: false },   // ← NEW
 }, { timestamps: true });
 
 module.exports = mongoose.model('ChatMsg', chatMsgSchema);
