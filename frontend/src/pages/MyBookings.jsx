559153ab26f89f5e686270980ade4edd0279a810
Juhikothari <juhikthr@gmail.com>
2026-08-08 09:29:24 +0530

    Add files via upload

--- frontend/src/pages/MyBookings.jsx
+++ frontend/src/pages/MyBookings.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
-import { createPortal } from 'react-dom';
 import { useAuth } from '../context/AuthContext.jsx';
 import * as api from '../services/api.js';
 import io from 'socket.io-client';
